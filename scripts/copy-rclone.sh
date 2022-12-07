48ccac08bdb92b816aeeef5433aa169f9870d6b4
Indira <indira@Indiras-MacBook-Pro.local>
2022-12-07 15:28:59 +0530

    added copy rclone script
    Signed-off-by: Indira <indira@Indiras-MacBook-Pro.local>

---
+++ scripts/copy-rclone.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "SRC :${SRC_PROVIDER}:${SRC_BUCKET}//${FILE_NAME} "
+echo "DEST: ${DEST_PROVIDER}:${DEST_BUCKET}//${FILE_NAME}"
+$BIN_DIR/rclone copy -v \
+    ${SRC_PROVIDER}:${SRC_BUCKET}//${FILE_NAME} \
+    ${DEST_PROVIDER}:${DEST_BUCKET}//${FILE_NAME}
+
+#rclone -v copy --checksum AWS:backupec2iam COS_SIV:aws-vm-transfer
